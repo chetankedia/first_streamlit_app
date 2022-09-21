e50ada4947a6f551b5e1d3ee74325abf5ed8acdd
chetankedia <41448103+chetankedia@users.noreply.github.com>
2022-09-21 12:02:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-streamlit.header("Fruityvice Fruit Advice !")
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -43,18 +43,24 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
 
-
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+# Snowflake-related functions
+def get_fruit_load_list()
+  with my my_cnx.cursor() as my_cur:
+  my_cur.execute("SELECT * from fruit_load_list")
+  return my_cur.fetchall()
 
+
+# Add button to load fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list()
+  streamlit.dataframe(my_data_row)  
+
+  streamlit.stop()
+
+  
 fruit_choice_add = streamlit.text_input('What fruit would you like to add?','jack_fruit')
 #streamlit.write('The user entered ', fruit_choice_add)
 
